cdc1b3da6e942953e604f24d4378b0bf7591d6b6
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2023-10-01 18:50:36 +0300

    Some random digging through the data

--- hist_data_analysis/preprocess.py
+++ hist_data_analysis/preprocess.py
@@ -31,3 +31,39 @@ df.drop( "Date", axis=1, inplace=True )
 df.drop( "Time", axis=1, inplace=True )
 
 print( df.shape )
+
+water_temp=["PVT_IN_TO_DHW","PVT_OUT_FROM_DHW","PVT_IN_TO_SOLAR_BUFFER",
+            "PVT_OUT_FROM_SOLAR_BUFFER","SOLAR_BUFFER_IN","SOLAR_BUFFER_OUT",
+            "BTES_TANK_IN","BTES_TANK_OUT","SOLAR_HEAT_REJECTION_IN",
+            "SOLAR_HEAT_REJECTION_OUT","WATER_IN_EVAP","WATER_OUT_EVAP",
+            "WATER_IN_COND","WATER_OUT_COND","SH1_IN","SH1_RETURN",
+            "AIR_HP_TO_BTES_TANK","DHW_INLET","DHW_OUTLET","DHW_BOTTOM",
+            "SH_INLET","SH_RETURN","PVT_IN","PVT_OUT"]
+
+other_temp=["OUTDOOR_TEMP","BTES_TANK","SOLAR_BUFFER_TANK",
+            "SH_BUFFER","DHW_BUFFER","INDOOR_TEMP"]
+
+pressure=["SUCTION_PRESSURE","DISCHARGE_PRESSURE","ECO_PRESSURE"]
+
+flow=["FLOW_EVAPORATOR","FLOW_CONDENSER","FLOW_DHW",
+      "FLOW_SOLAR_HEAT_REJECTION","FLOW_PVT","FLOW_FAN_COILS_INDOOR"]
+
+other=["POWER_HP","POWER_PVT","PYRANOMETER","Compressor_HZ"]
+
+control=["THREE_WAY_EVAP_OPERATION","THREE_WAY_COND_OPERATION",
+         "THREE_WAY_SOLAR_OPERATION","FOUR_WAY_VALVE",
+         "AIR_COOLED_COMMAND","Residential_office_mode","MODBUS_LOCAL"]
+
+# just checking that nothing is fogotten. +1 for datetime
+assert len(df.columns) == len(water_temp)+len(other_temp)+len(pressure)+len(flow)+len(other)+len(control)+1
+
+for col in other_temp:
+    for offset in [1,5,10]:
+        timestep = df.iloc[offset,0] - df.iloc[0,0]
+        dfPrev = df.shift( offset )
+        consec = ( df.DATETIME - dfPrev.DATETIME <= 2*timestep )
+        delta = np.abs(df[col]-dfPrev[col])
+        print( "Describe delta {} at offset {}:".format(col,offset) )
+        print( delta.describe() )
+
+#df.to_csv("aa.csv")
