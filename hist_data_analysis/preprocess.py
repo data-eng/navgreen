97e1d35f3bb8d5c5a2d3122a4692871b230040d5
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2023-10-09 22:52:08 +0300

    Some more on data preparation
    Outlying value filtering.
    Ingetsion in Influx.

--- hist_data_analysis/preprocess.py
+++ hist_data_analysis/preprocess.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
 df=pd.read_csv( "DATA_FROM_PLC.csv",
                 parse_dates=["Date&time"],
                 low_memory=False )
@@ -10,7 +13,8 @@ df.rename( columns={
         "3-WAY_SOLAR_OPERATION": "THREE_WAY_SOLAR_OPERATION",
         "4_WAY_VALVE": "FOUR_WAY_VALVE",
         "Date&time": "DATETIME" }, inplace=True )
-print( df.shape )
+
+print( "Finished loading CSV, shape: {}".format(df.shape) )
 
 # Marked as "not important"
 df.drop( "SPARE_NTC_SENSOR", axis=1, inplace=True )
@@ -30,7 +34,7 @@ df.drop( "EEV_LOAD2", axis=1, inplace=True )
 df.drop( "Date", axis=1, inplace=True )
 df.drop( "Time", axis=1, inplace=True )
 
-print( df.shape )
+print( "Finished preprocessing, shape: {}".format(df.shape) )
 
 water_temp=["PVT_IN_TO_DHW","PVT_OUT_FROM_DHW","PVT_IN_TO_SOLAR_BUFFER",
             "PVT_OUT_FROM_SOLAR_BUFFER","SOLAR_BUFFER_IN","SOLAR_BUFFER_OUT",
@@ -48,22 +52,75 @@ pressure=["SUCTION_PRESSURE","DISCHARGE_PRESSURE","ECO_PRESSURE"]
 flow=["FLOW_EVAPORATOR","FLOW_CONDENSER","FLOW_DHW",
       "FLOW_SOLAR_HEAT_REJECTION","FLOW_PVT","FLOW_FAN_COILS_INDOOR"]
 
-other=["POWER_HP","POWER_PVT","PYRANOMETER","Compressor_HZ"]
+power=["POWER_HP","POWER_PVT"]
+
+solar=["PYRANOMETER"]
+
+other=["Compressor_HZ"]
 
 control=["THREE_WAY_EVAP_OPERATION","THREE_WAY_COND_OPERATION",
          "THREE_WAY_SOLAR_OPERATION","FOUR_WAY_VALVE",
          "AIR_COOLED_COMMAND","Residential_office_mode","MODBUS_LOCAL"]
 
 # just checking that nothing is fogotten. +1 for datetime
-assert len(df.columns) == len(water_temp)+len(other_temp)+len(pressure)+len(flow)+len(other)+len(control)+1
+assert len(df.columns) == len(water_temp)+len(other_temp)+len(pressure)+len(flow)+len(power)+len(solar)+len(other)+len(control)+1
 
-for col in other_temp:
-    for offset in [1,5,10]:
-        timestep = df.iloc[offset,0] - df.iloc[0,0]
-        dfPrev = df.shift( offset )
-        consec = ( df.DATETIME - dfPrev.DATETIME <= 2*timestep )
-        delta = np.abs(df[col]-dfPrev[col])
-        print( "Describe delta {} at offset {}:".format(col,offset) )
-        print( delta.describe() )
+temp_sensors = []
+temp_sensors.extend( water_temp )
+temp_sensors.extend( other_temp )
 
-#df.to_csv("aa.csv")
+## Apply reasonable limits
+for col in temp_sensors:
+    df[col][(df[col]<-20)|(df[col]>100)] = np.nan
+for col in pressure:
+    df[col][(df[col]<0)|(df[col]>35)] = np.nan
+
+
+if 1==0:
+    for col in other_temp:
+        for offset in [1,5,10]:
+            timestep = df.iloc[offset,0] - df.iloc[0,0]
+            dfPrev = df.shift( offset )
+            consec = ( df.DATETIME - dfPrev.DATETIME <= 2*timestep )
+            delta = np.abs(df[col]-dfPrev[col])
+            print( "Describe delta {} at offset {}:".format(col,offset) )
+            print( delta.describe() )
+    df.to_csv("aa.csv")
+
+def make_point( measurement, row, columns ):
+    p = influxdb_client.Point( measurement )
+    p.time( df.loc[row,"DATETIME"].value, write_precision="ns" )
+    # Tag with the state of the valves, as context
+    for col in control:
+        if df.loc[row,col] is not np.nan:
+            p.tag( col, df.loc[row,col] )
+    # Add the sensor data fields
+    for col in columns:
+        if df.loc[row,col] is not np.nan:
+            p.field( col, df.loc[row,col] )
+    return p
+
+if 1==1:
+
+    client = influxdb_client.InfluxDBClient(
+        url="http://myurl.com:8086",
+        token="secret",
+        org="torg" )
+    api = client.write_api( write_options=SYNCHRONOUS )
+    for ind in df.index:
+        p = make_point( "temperature", ind, temp_sensors )
+        api.write( bucket="stasinos-playground", org="torg", record=p )
+        p = make_point( "pressure", ind, pressure )
+        api.write( bucket="stasinos-playground", org="torg", record=p )
+        p = make_point( "flow", ind, flow )
+        api.write( bucket="stasinos-playground", org="torg", record=p )
+        p = make_point( "flow", ind, flow )
+        api.write( bucket="stasinos-playground", org="torg", record=p )
+        p = make_point( "power", ind, power )
+        api.write( bucket="stasinos-playground", org="torg", record=p )
+        p = make_point( "solar", ind, solar )
+        api.write( bucket="stasinos-playground", org="torg", record=p )
+        p = make_point( "other", ind, other )
+        api.write( bucket="stasinos-playground", org="torg", record=p )
+        if ind % 1000 == 0:
+            print( "{} rows loaded".format(ind) )
