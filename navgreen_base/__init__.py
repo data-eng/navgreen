83a6983d8d9b89a690d344d4ea41d3644bc18922
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2023-11-06 21:27:39 +0200

    Minor fixes
    Fixed Influx client warning.
    Added tester script to pyproject.

--- navgreen_base/__init__.py
+++ navgreen_base/__init__.py
@@ -1,2 +1,4 @@
-import navgreen_base.influx
+import warnings
+from influxdb_client.client.warnings import MissingPivotFunction
+warnings.simplefilter("ignore", MissingPivotFunction)
 from navgreen_base.influx import make_point, write_data, read_data, delete_data
