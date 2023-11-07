78aa3d77475925a6322f7bad9714c893ebbc3214
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2023-11-07 05:26:42 +0200

    Minor correction in comment

--- scripts.py
+++ scripts.py
@@ -3,7 +3,7 @@ import subprocess
 def test():
     """
     Run all unittests. Equivalent to:
-    `poetry run python -u -m unittest discover`
+    `poetry run python -u -m unittest`
     """
     subprocess.run(
         ['python', '-u', '-m', 'unittest']
