901d9f090359ecfbafc1567056677d3a43ca5fbd
tatiana-boura <tat.boura@gmail.com>
2023-11-16 16:53:48 +0200

    Use pre-processing infrastructure for historical data analysis

--- hist_data_analysis/sampling_rate.py
+++ hist_data_analysis/sampling_rate.py
@@ -1,43 +1,11 @@
-import numpy as np
 import pandas as pd
+
+from navgreen_base import process_data
 
 from hist_data_analysis import similarity
 
 
 FIGURES_DIR = "figures"
-
-
-columns = ["PVT_IN_TO_DHW", "PVT_OUT_FROM_DHW",
-           "PVT_IN_TO_SOLAR_BUFFER", "PVT_OUT_FROM_SOLAR_BUFFER",
-           "SOLAR_BUFFER_IN", "SOLAR_BUFFER_OUT", "BTES_TANK_IN",
-           "BTES_TANK_OUT", "SOLAR_HEAT_REJECTION_IN",
-           "SOLAR_HEAT_REJECTION_OUT", "WATER_IN_EVAP",
-           "WATER_OUT_EVAP", "WATER_IN_COND", "WATER_OUT_COND",
-           "SH1_IN", "SH1_RETURN", "AIR_HP_TO_BTES_TANK",
-           "DHW_INLET", "DHW_OUTLET", "DHW_BOTTOM", "SH_INLET",
-           "SH_RETURN", "PVT_IN", "PVT_OUT"]
-
-def load_csv():
-    df = pd.read_csv( "data/DATA_FROM_PLC.csv",
-                      parse_dates=["Date&time"], low_memory=False )
-    df.rename(columns={
-            "3-WAY_EVAP_OPERATION": "THREE_WAY_EVAP_OPERATION",
-            "3-WAY_COND_OPERATION": "THREE_WAY_COND_OPERATION",
-            "3-WAY_SOLAR_OPERATION": "THREE_WAY_SOLAR_OPERATION",
-            "4_WAY_VALVE": "FOUR_WAY_VALVE",
-            "Date&time": "DATETIME",
-            "RECEIVER LIQUID_OUT": "RECEIVER_LIQUID_OUT"}, inplace=True)
-    # Marked as "not important"
-    df.drop("SPARE_NTC_SENSOR", axis=1, inplace=True)
-    df.drop("POWER_GLOBAL_SOL", axis=1, inplace=True)
-
-    # Redundant
-    df.drop("Date", axis=1, inplace=True)
-    df.drop("Time", axis=1, inplace=True)
-    return df
-#end def load_csv
-
-
 
 
 def undersampling_error( t, v ):
@@ -63,7 +31,9 @@ def undersampling_error( t, v ):
 
 
 def runme():
-    df = load_csv()
+    df = pd.read_csv("data/DATA_FROM_PLC.csv", parse_dates=["Date&time"], low_memory=False)
+    df = process_data(df, hist_data=True)
+
     dfQ4=df[(df['DATETIME'] > '2022-10-01') & (df['DATETIME'] < '2023-01-01')]
     err = None
     for col in ["PVT_IN_TO_DHW"]:
