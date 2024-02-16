050d81ea3a371b45acfc979cb23940600285679e
tatiana-boura <tat.boura@gmail.com>
2024-02-16 12:53:36 +0200

    Non-looping server

--- communication_PLC/server.py
+++ communication_PLC/server.py
@@ -14,47 +14,48 @@ server_socket.listen(1)
 print('Server listening on ' + str(server_host) + ':' + str(server_port))
 
 try:
-    while True:
+    setpoint_DHW = None
+    confirmation = None
 
-        # Incoming connection
+    while confirmation != "y":
+        setpoint_DHW = input("Please enter the desired value: ")
+
+        try:
+            # Only accept numerical values
+            setpoint_DHW = float(setpoint_DHW)
+
+            while True:
+                confirmation = input("Is the entered value the new DHW setpoint? (y/n): ").lower()
+
+                if confirmation == "y":
+                    print("Sending the value to the PLC when it connects.")
+                    break
+                elif confirmation == "n":
+                    print("Re-enter DHW setpoint.")
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
+
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+         # Incoming connection
         client_socket, client_address = server_socket.accept()
         print('Connection established with' + str(client_address))
 
-        setpoint_DHW = None
-        confirmation = None
+        try:
+            # If is number and user is sure about their choices, send setpoint to client
+            client_socket.send(str(setpoint_DHW).encode('utf-8'))
+            print("Setpoint sent.")
 
-        # Get the setpoint from the user - only check for numerical value.
-        # The PLC-related checks will be conducted from the client process
+        except Exception as e:
+            print("\nSending the setpoint failed: " + str(e))
 
-        while confirmation != "y":
-            setpoint_DHW = input("Please enter the desired value: ")
-
-            try:
-                # Only accept numerical values
-                setpoint_DHW = float(setpoint_DHW)
-
-                while True:
-                    confirmation = input("Is the entered value the new DHW setpoint? (y/n): ").lower()
-
-                    if confirmation == "y":
-                        print("Sending the value to the PLC.")
-                        break
-                    elif confirmation == "n":
-                        print("Re-enter DHW setpoint.")
-                        break
-                    else:
-                        print("Invalid input. Please enter 'y' or 'n'.")
-
-            except ValueError:
-                print("Invalid input. Please enter a number.")
-                continue
-
-        # If is number and user is sure about their choices, send setpoint to client
-        client_socket.send(str(setpoint_DHW).encode('utf-8'))
-
-        # Terminate connection
-        client_socket.close()
-        print('Closing connection with ' + str(client_address))
+        finally:
+            # Terminate connection
+            client_socket.close()
+            print('Closing connection with ' + str(client_address))
 
 except KeyboardInterrupt:
     # User performed Ctrl ^C
