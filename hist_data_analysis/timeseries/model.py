565f1ff6a0e2cd6e0253d323c70d1808b91fa6ee
tatiana-boura <tat.boura@gmail.com>
2024-03-12 11:45:04 +0200

    Simple LSTM model

---
+++ hist_data_analysis/timeseries/model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class LSTMRegressor(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTMRegressor, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])  # Take the output from the last time step
+        return out
