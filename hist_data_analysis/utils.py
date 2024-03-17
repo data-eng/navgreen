01ebfb18c1bf41bd379781c3b63f769a44e66fff
Natalia Koliou <natalykoliou@yahoo.gr>
2024-03-17 16:15:59 +0200

    Add more helper functions to utils.py

--- hist_data_analysis/utils.py
+++ hist_data_analysis/utils.py
@@ -15,3 +15,36 @@ def normalize(df):
             series = scipy.signal.detrend(series)
             newdf[col] = series
     return newdf
+
+def filter(df, columns, thresholds):
+    """
+    Filter dataframe based on multiple columns and their respective thresholds if the columns exist
+    :param df: dataframe
+    :param columns: list of column names to filter
+    :param thresholds: list of threshold values for filtering
+    :return: filtered dataframe if columns exist, otherwise original dataframe
+    """
+    for col, threshold in zip(columns, thresholds):
+        if col in df.columns:
+            if threshold is not None:
+                df = df[df[col] > threshold]
+            else:
+                df.drop(col, axis="columns", inplace=True)
+    return df
+
+def aggregate(df, grp, func):
+    """
+    Resample dataframe based on the provided frequency and aggregate using the specified function.
+    
+    :param df: dataframe
+    :param grp: resampling frequency
+    :param func: aggregation function
+    :return: aggregated dataframe
+    """
+    df = df.set_index("DATETIME")
+    if grp:
+        df = df.resample(grp)
+        df = df.apply(func)
+        df = df.dropna()
+    df = df.sort_index()
+    return df
