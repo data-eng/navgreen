6a1dceb066972a9020233767e4d5433b3100ada5
tatiana-boura <tat.boura@gmail.com>
2024-03-22 18:58:31 +0200

    Utilize masks in NN

--- hist_data_analysis/timeseries/model.py
+++ hist_data_analysis/timeseries/model.py
@@ -27,29 +27,35 @@ class MultiTimeAttention(nn.Module):
         self.h = num_heads
         self.dim = input_dim
         self.nhidden = nhidden
-
         self.linears = nn.ModuleList([nn.Linear(embed_time, embed_time),
                                       nn.Linear(embed_time, embed_time),
                                       nn.Linear(input_dim * num_heads, nhidden)])
 
-    def attention(self, query, key, value, dropout=None):
-        # Compute 'Scaled Dot Product Attention'
+
+    def attention(self, query, key, value, mask=None, dropout=None):
+        "Compute 'Scaled Dot Product Attention'"
         dim = value.size(-1)
         d_k = query.size(-1)
-        scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+        scores = torch.matmul(query, key.transpose(-2, -1)) \
+                 / math.sqrt(d_k)
         scores = scores.unsqueeze(-1).repeat_interleave(dim, dim=-1)
+        if mask is not None:
+            scores = scores.masked_fill(mask.unsqueeze(-3) == 0, -1e9)
         p_attn = F.softmax(scores, dim=-2)
         if dropout is not None:
             p_attn = dropout(p_attn)
         return torch.sum(p_attn * value.unsqueeze(-3), -2), p_attn
 
-    def forward(self, query, key, value, dropout=None):
+    def forward(self, query, key, value, mask=None, dropout=None):
         # Compute 'Scaled Dot Product Attention'
         batch, seq_len, dim = value.size()
+        if mask is not None:
+            # Same mask applied to all h heads.
+            mask = mask.unsqueeze(1)
         value = value.unsqueeze(1)
         query, key = [l(x).view(x.size(0), -1, self.h, self.embed_time_k).transpose(1, 2)
                       for l, x in zip(self.linears, (query, key))]
-        x, _ = self.attention(query, key, value, dropout)
+        x, _ = self.attention(query, key, value, mask, dropout)
         x = x.transpose(1, 2).contiguous() \
             .view(batch, -1, self.h * dim)
         return self.linears[-1](x)
@@ -99,6 +105,8 @@ class MtanGruRegr(nn.Module):
     def forward(self, x, time_steps):
         # x is: [batch_size, sequence_length, input_size]
         time_steps = time_steps.cpu()
+        mask = x[:, :, self.dim:]
+        mask = torch.cat((mask, mask), 2)
         if self.learn_emb:
             key = self.learn_time_embedding(time_steps).to(self.device)
             query = self.learn_time_embedding(self.query.unsqueeze(0)).to(self.device)
@@ -106,7 +114,7 @@ class MtanGruRegr(nn.Module):
             key = self.time_embedding(time_steps, self.embed_time).to(self.device)
             query = self.time_embedding(self.query.unsqueeze(0), self.embed_time).to(self.device)
 
-        out = self.att(query, key, x)
+        out = self.att(query, key, x, mask)
         out = out.permute(1, 0, 2)
         _, out = self.enc(out)
         return self.classifier(out.squeeze(0))
