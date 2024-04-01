4335a7417d89023028541fa3b8e2418d67f246b6
tatiana-boura <tat.boura@gmail.com>
2024-04-01 12:43:08 +0300

    Remove old LSTM approach

--- hist_data_analysis/mTAN/model.py
+++ hist_data_analysis/mTAN/model.py
@@ -4,18 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class LSTMRegressor(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
-        super(LSTMRegressor, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        out, _ = self.lstm(x)
-        out = self.fc(out[:, -1, :])  # Take the output from the last time step
-        return out
-
-
 class MultiTimeAttention(nn.Module):
 
     def __init__(self, input_dim, nhidden, embed_time, num_heads):
