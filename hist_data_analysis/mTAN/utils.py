c3377cc8c4a641fe6cb6ac8806fc674fa2db842c
tatiana-boura <tat.boura@gmail.com>
2024-04-10 17:13:07 +0300

    Update loss functions to handle embedding output

--- hist_data_analysis/mTAN/utils.py
+++ hist_data_analysis/mTAN/utils.py
@@ -3,25 +3,19 @@ import torch.nn as nn
 
 
 
-def mean_squared_error(orig, pred, mask):
-    error = (orig - pred) ** 2
-    error = error * mask
-    return error.sum() / mask.sum()
-
-
 class MaskedMSELoss(nn.Module):
-    def __init__(self, mask_value=0.):
+    def __init__(self, sequence_length):
         super(MaskedMSELoss, self).__init__()
-        self.mask_value = mask_value
+        self.sequence_length = sequence_length
 
-    def forward(self, input, target, mask):
+    def forward(self, pred, true, mask):
+        if pred.dim() == 1: pred = pred.unsqueeze(0)
+        pred = pred.view(pred.shape[0], self.sequence_length, pred.shape[1] // self.sequence_length).mean(dim=2)
         # Compute element-wise squared difference
-        squared_diff = (input - target) ** 2
+        squared_diff = (pred - true) ** 2
         # Apply mask to ignore certain elements
         mask = mask.float()
-        squared_diff = squared_diff * mask
-        # Compute loss
-        loss = squared_diff
+        loss = squared_diff * mask
         # Compute the mean loss only over non-masked elements
         loss = loss.sum() / (mask.sum() + 1e-8)
 
@@ -29,13 +23,17 @@ class MaskedMSELoss(nn.Module):
 
 
 class MaskedSmoothL1Loss(nn.Module):
-    def __init__(self, mask_value=0.):
+    def __init__(self, sequence_length):
         super(MaskedSmoothL1Loss, self).__init__()
-        self.mask_value = mask_value
+        self.sequence_length = sequence_length
 
-    def forward(self, input, target, mask):
+    def forward(self, pred, true, mask):
+        '''
+        if pred.dim() == 1: pred = pred.unsqueeze(0)
+        pred = pred.view(pred.shape[0], self.sequence_length, pred.shape[1] // self.sequence_length).mean(dim=2)
+        '''
         # Compute element-wise absolute difference
-        abs_diff = torch.abs(input - target)
+        abs_diff = torch.abs(pred - true)
         # Apply mask to ignore certain elements
         mask = mask.float()
         abs_diff = abs_diff * mask
