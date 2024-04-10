63f061851f707225c20df9100ec3348412f5de43
tatiana-boura <tat.boura@gmail.com>
2024-04-10 17:12:39 +0300

    RNN and Regressor output aligned with time embedding

--- hist_data_analysis/mTAN/model.py
+++ hist_data_analysis/mTAN/model.py
@@ -6,18 +6,16 @@ import torch.nn.functional as F
 
 class MultiTimeAttention(nn.Module):
 
-    def __init__(self, input_dim, nhidden, embed_time, num_heads):
+    def __init__(self, input_dim, embed_time, num_heads):
         super(MultiTimeAttention, self).__init__()
         assert embed_time % num_heads == 0
         self.embed_time = embed_time
         self.embed_time_k = embed_time // num_heads
         self.h = num_heads
         self.dim = input_dim
-        self.nhidden = nhidden
         self.linears = nn.ModuleList([nn.Linear(embed_time, embed_time),
                                       nn.Linear(embed_time, embed_time),
-                                      nn.Linear(input_dim * num_heads, nhidden)])
-
+                                      nn.Linear(input_dim * num_heads, embed_time)])
 
     def attention(self, query, key, value, mask=None, dropout=None):
         "Compute 'Scaled Dot Product Attention'"
@@ -47,63 +45,22 @@ class MultiTimeAttention(nn.Module):
         return x
 
 
-class MtanGruRegr(nn.Module):
+class MtanRNNRegr(nn.Module):
 
-    def __init__(self, input_dim, query, device, nhidden, embed_time, num_heads, output_len=2 ):
-        super(MtanGruRegr, self).__init__()
+    def __init__(self, input_dim, query, device, embed_time, num_heads):
+        super(MtanRNNRegr, self).__init__()
         assert embed_time % num_heads == 0
         self.device = device
         self.embed_time = embed_time
-        self.nhidden = nhidden
         self.query = query
-        self.att = MultiTimeAttention(2 * input_dim, nhidden, embed_time, num_heads)
+        self.att = MultiTimeAttention(2 * input_dim, embed_time, num_heads)
         # self.att = MultiTimeAttention(input_dim, nhidden, embed_time, num_heads)
         self.regressor = nn.Sequential(
-            nn.Linear(nhidden, 128),
+            nn.Linear(embed_time, 128),
             nn.Linear(128, 128),
-            nn.Linear(128, output_len))
-        self.enc = nn.RNN(nhidden, nhidden, batch_first=True)
-
-        self.periodic = nn.Linear(1, embed_time - 1)
-        self.linear = nn.Linear(1, 1)
-
-    def learn_time_embedding(self, tt):
-        tt = tt.to(self.device)
-        tt = tt.unsqueeze(-1)
-        out2 = torch.sin(self.periodic(tt))
-        out1 = self.linear(tt)
-        return torch.cat([out1, out2], -1)
-
-    def forward(self, x, time_steps, mask):
-        # x is: [batch_size, sequence_length, input_size]
-        x = torch.cat((x, mask), 2)
-        mask = torch.cat((mask, mask), 2)
-        time_steps = time_steps.to(self.device)
-
-        key = self.learn_time_embedding(time_steps).to(self.device)
-        query = self.learn_time_embedding(self.query.unsqueeze(0)).to(self.device)
-
-        out = self.att(query, key, x, mask)
-        _, out = self.enc(out)
-        out = out.squeeze()
-        return self.regressor(out.squeeze(0))
-
-
-class MtanGruClassif(nn.Module):
-
-    def __init__(self, input_dim, query, device, nhidden, embed_time, num_heads, output_len=2 ):
-        super(MtanGruClassif, self).__init__()
-        assert embed_time % num_heads == 0
-        self.device = device
-        self.embed_time = embed_time
-        self.nhidden = nhidden
-        self.query = query
-        self.att = MultiTimeAttention(2 * input_dim, nhidden, embed_time, num_heads)
-        self.classifier = nn.Sequential(
-            nn.Linear(nhidden, 128),
-            nn.Linear(128, 128),
-            nn.Linear(128, output_len))
-        self.enc = nn.RNN(nhidden, nhidden, batch_first=True)
+            nn.Linear(128, embed_time),
+            nn.Linear(embed_time, 8))
+        self.enc = nn.RNN(embed_time, embed_time, batch_first=True)
 
         self.periodic = nn.Linear(1, embed_time - 1)
         self.linear = nn.Linear(1, 1)
