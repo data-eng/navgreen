a3e41dedfed83d1caa33de149fcb9dead7380652
tatiana-boura <tat.boura@gmail.com>
2024-04-11 16:47:24 +0300

    Custom masked cross entropy loss

--- hist_data_analysis/mTAN/utils.py
+++ hist_data_analysis/mTAN/utils.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 
 class MaskedMSELoss(nn.Module):
@@ -41,5 +41,33 @@ class MaskedSmoothL1Loss(nn.Module):
         loss = torch.where(abs_diff < 1, 0.5 * abs_diff ** 2, abs_diff - 0.5)
         # Compute the mean loss only over non-masked elements
         loss = loss.sum() / (mask.sum() + 1e-8)  # Add epsilon to avoid division by zero
+
+        return loss
+
+
+class MaskedCrossEntropyLoss(nn.Module):
+    def __init__(self, sequence_length):
+        super(MaskedCrossEntropyLoss, self).__init__()
+        self.sequence_length = sequence_length
+
+    def forward(self, pred, true, mask):
+
+        if pred.dim() == 2: pred = pred.permute(1,0).unsqueeze(0)
+        pred = pred.view(pred.shape[0], self.sequence_length, pred.shape[1] // self.sequence_length,
+                         pred.shape[2]).mean(dim=2)
+
+        softmax_output = torch.softmax(pred, dim=-1)
+        pred = softmax_output
+
+        true = true.long()
+        true = true * mask.long()
+        loss = [F.cross_entropy(pred[b_sz, :, :], true[b_sz, :], reduction='none') for b_sz in range(true.shape[0])]
+        loss = torch.stack(loss, dim=0)
+        loss = torch.mean(loss, dim=0)
+
+        mask = mask.float()
+        loss = loss * mask
+
+        loss = torch.sum(loss) / torch.sum(mask)
 
         return loss
