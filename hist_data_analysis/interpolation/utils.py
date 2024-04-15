47e51ee0250e5dfbab1e3eb22f78c81ccd8b715a
tatiana-boura <tat.boura@gmail.com>
2024-04-15 15:12:00 +0300

    Add class weights to loss function

--- hist_data_analysis/interpolation/utils.py
+++ hist_data_analysis/interpolation/utils.py
@@ -4,14 +4,20 @@ import torch.nn.functional as F
 
 
 class CrossEntropyLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, weights):
         super(CrossEntropyLoss, self).__init__()
+        w = 1.0 / weights
+        self.weights = w / w.sum()
+        print(f'weights is {self.weights}')
 
-    def forward(self, pred, true):
-        if pred.dim() == 2: pred = pred.permute(1,0).unsqueeze(0)
+    def forward(self, pred, true, mask):
+        if pred.dim() == 2: pred = pred.permute(1, 0).unsqueeze(0)
         true = true.long()
-        loss = [F.cross_entropy(pred[b_sz, :, :], true[b_sz, :], reduction='none') for b_sz in range(true.shape[0])]
+        true = true * mask.long()
+        loss = [F.cross_entropy(pred[b_sz, :, :], true[b_sz, :], reduction='none', weight=self.weights) for b_sz in range(true.shape[0])]
         loss = torch.stack(loss, dim=0)
-        loss = torch.sum(loss)
+        mask = mask.float()
+        loss = loss * mask
+        loss = torch.sum(loss) / torch.sum(mask)
 
-        return loss / (loss.shape[0] * loss.shape[1])
+        return loss
