cb7c4011ba96901c45c664e1f00f02ac3ff83836
tatiana-boura <tat.boura@gmail.com>
2024-12-16 14:45:03 +0200

    Perform testing

--- time_representations/public_weather_meteo/hist_data_analysis/transformer/test.py
+++ time_representations/public_weather_meteo/hist_data_analysis/transformer/test.py
@@ -14,10 +14,12 @@ logger.addHandler(stream_handler)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 logger.info(f'Device is {device}')
 
+params_init = {"X": ["humidity", "pressure", "feels_like", "temp", "wind_speed", "rain_1h"]}
 
-def test(data, df, classes, criterion, model, seed, y, visualize=True):
+
+def test(data, df, classes, criterion, model, seed, y, dir_name, visualize=True):
     ylabel = y[0]
-    mfn = utils.get_path(dirs=["models", ylabel, "transformer", str(seed)], name="transformer.pth")
+    mfn = utils.get_path(dirs=["models", ylabel, "transformer", dir_name, str(seed)], name="transformer.pth")
     model.load_state_dict(torch.load(mfn))
 
     model.to(device)
@@ -75,10 +77,10 @@ def test(data, df, classes, criterion, model, seed, y, visualize=True):
                         title="Test Heatmap "+ylabel,
                         classes=classes,
                         coloring=['azure', 'darkblue'],
-                        path=utils.get_path(dirs=["models", ylabel, "transformer", str(seed)]))
+                        path=utils.get_path(dirs=["models", ylabel, "transformer", dir_name, str(seed)]))
         
     checkpoints.update({'test_loss': avg_test_loss, **utils.get_prfs(true=true_classes, pred=pred_classes)})
-    cfn = utils.get_path(dirs=["models", ylabel, "transformer", str(seed)], name="test_checkpoints.json")
+    cfn = utils.get_path(dirs=["models", ylabel, "transformer", dir_name, str(seed)], name="test_checkpoints.json")
     utils.save_json(data=checkpoints, filename=cfn)
 
     predicted_values_prob = np.exp(pred_values) / np.sum(np.exp(pred_values), axis=-1, keepdims=True)
@@ -94,7 +96,7 @@ def test(data, df, classes, criterion, model, seed, y, visualize=True):
          f"{ylabel}_pred": pred_classes,
          f"{ylabel}_probs": predicted_values_prob.tolist()}
 
-    dfn = utils.get_path(dirs=["models", ylabel, "transformer", str(seed)], name="data.csv")
+    dfn = utils.get_path(dirs=["models", ylabel, "transformer", dir_name, str(seed)], name="data.csv")
     utils.save_csv(data=data_, filename=dfn)
 
     logger.info(f'\nTesting with seed {seed} complete!\nTesting Loss: {avg_test_loss:.6f}\n')
@@ -103,33 +105,42 @@ def test(data, df, classes, criterion, model, seed, y, visualize=True):
 
 
 def main_loop(seed, y_col):
+    utils.set_seed(seed)
+
     path = "../../../data/test_classif_meteo.csv"
     seq_len = 24 // 3
     batch_size = 1
     classes = ["0", "1", "2", "3", "4"]
 
-    df = load(path=path, parse_dates=["DATETIME"], normalize=True, bin=y_col[0])
-    df_prep = prepare(df, phase="test")
+    for dir_name in ['tuned']:
+        df = load(path=path, parse_dates=["DATETIME"], normalize=True, bin=y_col[0])
+        df_prep = prepare(df, phase="test")
 
-    weights = utils.load_json(filename=f'transformer/weights_{y_col[0]}.json')
+        weights = utils.load_json(filename=f'transformer/weights_{y_col[0]}.json')
 
-    ds_test = TSDataset(df=df_prep, seq_len=seq_len, X=params["X"], t=params["t"], y=y_col, per_day=True)
-    dl_test = DataLoader(ds_test, batch_size, shuffle=False)
+        if dir_name == 'trained_new':
+            ds_test = TSDataset(df=df_prep, seq_len=seq_len, X=params["X"], t=params["t"], y=y_col, per_day=True)
+        else:
+            ds_test = TSDataset(df=df_prep, seq_len=seq_len, X=params["X"], t=params["t"], y=y_col,
+                                per_day=True, tune=True)
 
-    torch.manual_seed(seed)
+        dl_test = DataLoader(ds_test, batch_size, shuffle=False)
 
-    model = Transformer(in_size=len(params["X"])+len(params["t"]),
-                        out_size=len(classes),
-                        nhead=1,
-                        num_layers=1,
-                        dim_feedforward=2048,
-                        dropout=0)
+        X_size = len(params["X"]) if dir_name == 'trained_new' else len(params_init["X"])
 
-    _ = test(data=dl_test,
-             df=df_prep,
-             classes=classes,
-             criterion=utils.WeightedCrossEntropyLoss(weights),
-             model=model,
-             seed=seed,
-             y=y_col,
-             visualize=True)
+        model = Transformer(in_size=X_size+len(params["t"]),
+                            out_size=len(classes),
+                            nhead=1,
+                            num_layers=1,
+                            dim_feedforward=2048,
+                            dropout=0)
+
+        _ = test(data=dl_test,
+                 df=df_prep,
+                 classes=classes,
+                 criterion=utils.WeightedCrossEntropyLoss(weights),
+                 model=model,
+                 seed=seed,
+                 y=y_col,
+                 visualize=True,
+                 dir_name=dir_name)
